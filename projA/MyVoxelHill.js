37c0d7f9007d3bbea43bb2f6ffa17b5c7a766129
Mário Gil <mg_mesquita@hotmail.com>
2019-03-21 20:56:52 +0000

    Only draws surface cubes

--- projA/MyVoxelHill.js
+++ projA/MyVoxelHill.js
@@ -14,18 +14,20 @@ class MyVoxelHill extends CGFobject {
 
 			let start_x = cube_size/2.0 - cube_size * cubes_side/2.0;
 			let start_z = cube_size/2.0 - cube_size * cubes_side/2.0;
+			let end_x = -start_x;
+			let end_z = -start_z;
 
-			for (let i = 0; i < cubes_side; i++) {
-				let curr_z = start_z;
-				for (let j = 0; j < cubes_side; j++) {
+			for (let curr_x = start_x; curr_x <= end_x; curr_x += cube_size) {
+				for (let curr_z = start_z; curr_z <= end_z; curr_z += cube_size) {
+
+					/* Remove coment below to fully draw first layer */
+					if (/*curr_y != 0.5 &&*/ curr_x != start_x && curr_x != end_x && curr_z != start_z && curr_z != end_z) continue;
+
 					this.scene.pushMatrix();
-					this.scene.translate(start_x, curr_y, curr_z);
+					this.scene.translate(curr_x, curr_y, curr_z);
 					this.cube.display();
 					this.scene.popMatrix();
-
-					curr_z += cube_size;
 				}
-				start_x += cube_size;
 			}
 			curr_y += cube_size;
 		}
