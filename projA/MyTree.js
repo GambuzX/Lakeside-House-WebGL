a224ca43d1dbb3d453e6bad408b24dfdc9821807
Mário Gil <mg_mesquita@hotmail.com>
2019-03-28 16:39:01 +0000

    trees textures fix

--- projA/MyTree.js
+++ projA/MyTree.js
@@ -15,18 +15,18 @@ class MyTree extends CGFobject {
 
     initTreeMaterials() {
         this.trunkMat = new CGFappearance(this.scene);
-        this.trunkMat.setAmbient(1, 1, 1, 1);
+        this.trunkMat.setAmbient(0.1, 0.1, 0.1, 1);
         this.trunkMat.setDiffuse(1, 1, 1, 1);
         this.trunkMat.setSpecular(0, 0, 0, 1);
-        this.trunkMat.setShininess(5);
+        this.trunkMat.setShininess(1);
         this.trunkMat.loadTexture('textures/trees/tree_bark.png');
         this.trunkMat.setTextureWrap('REPEAT', 'REPEAT');
 
         this.leavesMat = new CGFappearance(this.scene);
-        this.leavesMat.setAmbient(1, 1, 1, 1);
+        this.leavesMat.setAmbient(0.1, 0.1, 0.1, 1);
         this.leavesMat.setDiffuse(1, 1, 1, 1);
         this.leavesMat.setSpecular(0, 0, 0, 1);
-        this.leavesMat.setShininess(5);
+        this.leavesMat.setShininess(1);
         this.leavesMat.loadTexture('textures/trees/leaves.jpg');
         this.leavesMat.setTextureWrap('REPEAT', 'REPEAT');
     }
