21c7e754bd72b741865e49f05b5db9e611a2db5b
Mário Gil <mg_mesquita@hotmail.com>
2019-03-28 16:37:26 +0000

    Trees textures

--- projA/MyTree.js
+++ projA/MyTree.js
@@ -10,6 +10,25 @@ class MyTree extends CGFobject {
 
         this.trunk = new MyCylinder(scene, 10);
         this.top = new MyCone(scene, 10, []);
+        this.initTreeMaterials();
+    }
+
+    initTreeMaterials() {
+        this.trunkMat = new CGFappearance(this.scene);
+        this.trunkMat.setAmbient(1, 1, 1, 1);
+        this.trunkMat.setDiffuse(1, 1, 1, 1);
+        this.trunkMat.setSpecular(0, 0, 0, 1);
+        this.trunkMat.setShininess(5);
+        this.trunkMat.loadTexture('textures/trees/tree_bark.png');
+        this.trunkMat.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.leavesMat = new CGFappearance(this.scene);
+        this.leavesMat.setAmbient(1, 1, 1, 1);
+        this.leavesMat.setDiffuse(1, 1, 1, 1);
+        this.leavesMat.setSpecular(0, 0, 0, 1);
+        this.leavesMat.setShininess(5);
+        this.leavesMat.loadTexture('textures/trees/leaves.jpg');
+        this.leavesMat.setTextureWrap('REPEAT', 'REPEAT');
     }
 
     enableNormalViz() {
@@ -20,12 +39,14 @@ class MyTree extends CGFobject {
     display() {
         this.scene.pushMatrix();
         this.scene.scale(this.trunkRadius, this.trunkHeight, this.trunkRadius);
+        this.trunkMat.apply();
         this.trunk.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(0,this.trunkHeight,0);
         this.scene.scale(this.treeTopRadius, this.treeTopHeight, this.treeTopRadius);
+        this.leavesMat.apply();
         this.top.display();
         this.scene.popMatrix();
     }
