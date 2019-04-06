8589b6fedf868525147565f3e3dfa68b653c2690
Moisés Rocha <moisesrocha99@gmail.com>
2019-04-06 11:21:33 +0100

    Tree group objects now generate trees with varied sizes

--- projA/MyTreeGroupPatch.js
+++ projA/MyTreeGroupPatch.js
@@ -9,10 +9,21 @@ class MyTreeGroupPatch extends CGFobject {
         this.offsets = [];
 
         /* Create trees */
+        let trunkHeight = 2;
+        let trunkRadius = 0.5;
+        let topheight = 2;
+        let topRadius = 1.25;
         for (let i = 0; i < 3; ++i) {
             this.trees.push([]);
             for (let j = 0; j < 3; ++j) {
-                this.trees[i].push(new MyTree(this.scene, 2,0.5,2,1.25,treeTrunkTex,treeTopTex));
+                let trunkHeightOff = Math.random() * 0.5 - 0.25;
+                let trunkRadiusOff = Math.random() * 0.25 - 0.125;
+                let topHeightOff = Math.random() * 0.5 - 0.25;
+                let topRadiusOff = Math.random() * 0.25 - 0.125;
+                this.trees[i].push(new MyTree(
+                    this.scene, trunkHeight + trunkHeightOff,
+                    trunkRadius + trunkRadiusOff, topheight + topHeightOff,
+                    topRadius + topRadiusOff, treeTrunkTex, treeTopTex));
             }
         }
 
@@ -20,16 +31,19 @@ class MyTreeGroupPatch extends CGFobject {
         for (let i = 0; i < 3; ++i) {
             this.offsets.push([]);
             for (let j = 0; j < 3; ++j) {
-                this.offsets[i].push([Math.random()*0.5-0.25, Math.random()*0.5-0.25]);
+                this.offsets[i].push(
+                    [Math.random() * 0.5 - 0.25, Math.random() * 0.5 - 0.25]);
             }
-        }        
+        }
     }
 
     display() {
         for (let i = 0; i < 3; ++i) {
             for (let j = 0; j < 3; ++j) {
                 this.scene.pushMatrix();
-                this.scene.translate(-3 + 3*i+this.offsets[i][j][0],0,-3 + 3*j+this.offsets[i][j][1]);
+                this.scene.translate(
+                    -3 + 3 * i + this.offsets[i][j][0], 0,
+                    -3 + 3 * j + this.offsets[i][j][1]);
                 this.trees[i][j].display();
                 this.scene.popMatrix();
             }
@@ -37,6 +51,7 @@ class MyTreeGroupPatch extends CGFobject {
     }
 
     enableNormalViz() {
-        for (let i = 0; i < this.trees.length; ++i) this.tress[i].enableNormalViz();
+        for (let i = 0; i < this.trees.length; ++i)
+            this.tress[i].enableNormalViz();
     }
 }
--- projA/MyTreeRowPatch.js
+++ projA/MyTreeRowPatch.js
@@ -8,28 +8,39 @@ class MyTreeRowPatch extends CGFobject {
         this.trees = [];
         this.offsets = [];
 
+        let trunkHeight = 2;
+        let trunkRadius = 0.5;
+        let topheight = 2;
+        let topRadius = 1.25;
         /* Create trees */
         for (let i = 0; i < 6; ++i) {
-            this.trees.push(new MyTree(this.scene, 2,0.5,2,1.5,treeTrunkTex,treeTopTex));
+            let trunkHeightOff = Math.random() * 0.5 - 0.25;
+            let trunkRadiusOff = Math.random() * 0.25 - 0.125;
+            let topHeightOff = Math.random() * 0.5 - 0.25;
+            let topRadiusOff = Math.random() * 0.25 - 0.125;
+            this.trees.push(new MyTree(
+                this.scene, trunkHeight + trunkHeightOff,
+                trunkRadius + trunkRadiusOff, topheight + topHeightOff,
+                topRadius + topRadiusOff, treeTrunkTex, treeTopTex));
         }
 
         /* Calculate offsets */
         for (let i = 0; i < 6; ++i) {
-            this.offsets.push((Math.random()-0.5)*2);
+            this.offsets.push((Math.random() - 0.5) * 2);
         }
-
     }
 
     display() {
         for (let i = 0; i < this.trees.length; ++i) {
             this.scene.pushMatrix();
-            this.scene.translate(this.offsets[i],0,-7.5+3*i);
+            this.scene.translate(this.offsets[i], 0, -7.5 + 3 * i);
             this.trees[i].display();
             this.scene.popMatrix();
         }
     }
 
     enableNormalViz() {
-        for (let i = 0; i < this.trees.length; ++i) this.tress[i].enableNormalViz();
+        for (let i = 0; i < this.trees.length; ++i)
+            this.tress[i].enableNormalViz();
     }
 }
